812dd5677272040ca9b2f0a52b0dd93568e1d693
Joseph Chavez <70075855+JosephChavxz@users.noreply.github.com>
2024-03-05 16:50:08 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+
+token_regex = {
+    'keyword': r'while',
+    'identifier': r'[a-zA-Z_]\w*',
+    'operator': r'[=<>]',
+    'separator': r'[();]',
+    'real': r'\d+\.\d+'
+}
+
+def lexer(input_file):
+    tokens = []
+
+    with open(input_file, 'r') as file:
+        source_code = file.read()
+
+    for token_type, regex in token_regex.items():
+        for match in re.finditer(regex, source_code):
+            tokens.append((token_type, match.group()))
+
+    return tokens
+
+def print_tokens(tokens):
+    print("Token\tLexeme")
+    for token, lexeme in tokens:
+        print(f"{token}\t{lexeme}")
+
+if __name__ == "__main__":
+    input_file = "323.txt"
+    tokens = lexer(input_file)
+    print_tokens(tokens)
